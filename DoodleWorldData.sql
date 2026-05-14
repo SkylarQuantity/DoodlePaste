dc2706a00fc654f4eb65316b5b23174b4018974c
SkylarQuantity <skylarshaymin@gmail.com>
2026-05-14 11:40:14 +0800

    Add files via upload

---
+++ DoodleWorldData.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE DoodleWorldData
+
+CREATE TABLE Doodles(
+DoodleName varchar(255) NOT NULL,
+PrimaryType varchar(255) NOT NULL
+);
+
+CREATE TABLE Moves(
+    MoveName varchar(255) NOT NULL,
+    MoveType varchar(255) NOT NULL
+);
